e7059af4ba6a271455256810282f25111dfcff92
wangyanxin <johndoe@example.com>
2017-09-04 09:36:02 +0800

    使用原模版进行开发
    使用原模版进行开发，减少导航及内容页面的开发与维护功能

---
+++ developerCommunity/js/common/router.js
@@ -0,0 +1,26 @@
+﻿// ================================================================
+//  author:文霞
+//  createDate: 2016/09/05
+//  description: 统计图表组件 路由定义
+//  ===============================================================
+define(function (require) {
+    "use strict";
+    return Backbone.Router.extend({
+        routes: {
+            "": "demo"
+        },
+        goIndex: function (requirePath, operationType, currentId, jsonObject) {
+            require([requirePath], function (view) {
+//      		var viewObj=(!currentId)?{model: {_opType:operationType}}:{model: {_opType:operationType,_currentId:currentId}};
+                var viewObj = {model: {_opType: operationType, _currentId: currentId, _jsonObject: jsonObject}};
+                var _view = new view(viewObj);
+                $('.page-content-wrapper').html(_view.$el);
+                //设置中间内容区域屏幕的高度,中间内容区域层的class必须是page-content
+                _view.afterRender();
+            });
+        },
+        demo:function(){
+            this.goIndex("pages/demoView");
+        }
+    })
+})
